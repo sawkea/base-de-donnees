e3e47ca2dfc7c81b6a429ad363ad880573dbcd43
Sawkea <shoareau@orange.fr>
2020-06-10 22:30:46 +0200

    Updates Affichage des apprenants ACS et leur départements avec récupération de la base de données sur le serveur

--- index.php
+++ index.php
@@ -8,14 +8,32 @@
 <body>
     <?php
 
+
+    $servername = "localhost";
+    $username = "soniah";
+    $password = "mW3SxOh6/3S+gQ==";
+    
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=apprenants', 'root', '');
-        $bdd->exec('SET NAMES utf8');
-        var_dump ($bdd);    
-    } catch (PDOException $e) {
-        $msg = 'ERREUR PDO dans ' . $e->getFile() . ' Ligne.' . $e->getLine() . ' : <br>' . $e->getMessage();
-        die($msg);
-        }
+      $conn = new PDO("mysql:host=$servername;dbname=soniah_base-de-donnees", $username, $password);
+      $conn->exec('SET NAMES utf8');
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected successfully";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+
+
+    // try {
+    //     $bdd = new PDO('mysql:host=localhost;dbname=apprenants', 'root', '');
+    //     $bdd->exec('SET NAMES utf8');
+    //     var_dump ($bdd);    
+    // } catch (PDOException $e) {
+    //     $msg = 'ERREUR PDO dans ' . $e->getFile() . ' Ligne.' . $e->getLine() . ' : <br>' . $e->getMessage();
+    //     die($msg);
+    //     }
+
+
 
         // $reponse = $bdd->query('SELECT * FROM table_1');
         // while ($donnees = $reponse->fetch()){
@@ -30,7 +48,7 @@
         echo "<h1>ACS de besançon</h1> 
                 <h2>Liste des apprenants et leur département de résidence</h2>";
                 
-            $reponse = $bdd->query('SELECT Nom, nom_departement FROM membres_acs INNER JOIN departements_acs ON membres_acs.num_departement = departements_acs.num_departement');
+            $reponse = $conn->query('SELECT Nom, nom_departement FROM membres_acs INNER JOIN departements_acs ON membres_acs.num_departement = departements_acs.num_departement');
             while ($donnees = $reponse->fetch()){
                echo '<p>' . $donnees['Nom'] . ' - ' . $donnees['nom_departement'] . '</p>';
             }
