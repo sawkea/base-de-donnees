8a01364f7b0e0f9c469fd6da9eea8aae98046099
Sawkea <shoareau@orange.fr>
2020-06-10 15:38:17 +0200

    Updates affichage apprenants ACS Besançon

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
     try {
         $bdd = new PDO('mysql:host=localhost;dbname=apprenants', 'root', '');
+        $bdd->exec('SET NAMES utf8');
         var_dump ($bdd);    
     } catch (PDOException $e) {
         $msg = 'ERREUR PDO dans ' . $e->getFile() . ' Ligne.' . $e->getLine() . ' : <br>' . $e->getMessage();
@@ -21,11 +22,20 @@
         //     echo '<p>' . $donnees['id'] . '</p>';
 
         // }
-        $reponse = $bdd->query('SELECT * FROM table_1 WHERE last_name="palmer"');
-        while ($donnees = $reponse->fetch()){
-            echo '<p>' . $donnees['id'] . ' - ' . $donnees['last_name'] . '</p>';
+        // $reponse = $bdd->query('SELECT * FROM table_1 WHERE last_name="palmer"');
+        // while ($donnees = $reponse->fetch()){
+        //     echo '<p>' . $donnees['id'] . ' - ' . $donnees['last_name'] . '</p>';
 
-        }
-    ?>
+        // }
+        echo "<h1>ACS de besançon</h1> 
+                <h2>Liste des apprenants et leur département de résidence</h2>";
+                
+            $reponse = $bdd->query('SELECT Nom, nom_departement FROM membres_acs INNER JOIN departements_acs ON membres_acs.num_departement = departements_acs.num_departement');
+            while ($donnees = $reponse->fetch()){
+               echo '<p>' . $donnees['Nom'] . ' - ' . $donnees['nom_departement'] . '</p>';
+            }
+//         SELECT * FROM membres_acs INNER JOIN departements_acs ON membres_acs.num_departement = departements_acs.num_departement
+// SELECT Nom, nom_departement FROM membres_acs INNER JOIN departements_acs ON membres_acs.num_departement = departements_acs.num_departement
+?>
 </body>
 </html>
